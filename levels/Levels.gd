185dbba21ed415b9ead4daa2b28eb3d192b04efe
AdamSmif <35468207+AdamSmif@users.noreply.github.com>
2021-06-06 16:10:34 -0700

    added dialog at Joe Sanders House

--- levels/Levels.gd
+++ levels/Levels.gd
@@ -4,4 +4,4 @@ extends Node2D
 
 func _ready():
 	if Global.from != null:
-d		get_node("Player").set_position(get_node(Global.from + "Pos").position)
+		get_node("Player").set_position(get_node(Global.from + "Pos").position)
