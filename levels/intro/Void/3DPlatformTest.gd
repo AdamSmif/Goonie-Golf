a4199ebaafe4c4762ada84810b702c748e49adb2
AdamSmif <35468207+AdamSmif@users.noreply.github.com>
2021-06-22 22:09:45 -0700

    added 3D level + start of void

---
+++ levels/intro/Void/3DPlatformTest.gd
@@ -0,0 +1,11 @@
+extends Spatial
+
+export(String, FILE, "*.tscn") var next_world_scene
+
+func _ready():
+	yield(get_tree().create_timer(15), "timeout")
+	get_tree().change_scene(next_world_scene)
+	
+#func _process(delta):
+#	if Input.is_action_just_pressed("ui_accept"):
+#		get_tree().change_scene(next_world_scene)
