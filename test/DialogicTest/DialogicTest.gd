a31ae5cc36d116271869a7a60911169a5caf812d
AdamSmif <35468207+AdamSmif@users.noreply.github.com>
2021-06-30 21:29:56 -0700

    fixed bugs readded dialogic but current not working

--- test/DialogicTest/DialogicTest.gd
+++ test/DialogicTest/DialogicTest.gd
@@ -1,12 +1,7 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	if Global.from != null:
-		get_node("Player").set_position(get_node(Global.from + "Pos").position)
+	var dialog = Dialogic.start("talk1")
+	add_child(dialog)
