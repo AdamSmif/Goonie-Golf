3851b9da697b2566cb3ac054b5578e435d6d6db6
AdamSmif <35468207+AdamSmif@users.noreply.github.com>
2021-07-21 20:28:24 -0700

    added stuff

--- levels/Project Disc Golf1/intro-cutscene/IntroCutscene.gd
+++ levels/Project Disc Golf1/intro-cutscene/IntroCutscene.gd
@@ -1,6 +1,6 @@
 extends Control
 
-export(String, FILE, "*.tscn") var next_world_scene
+export(String, FILE, "*.tscn") var next_scene
 
 func _ready():
 	MusicController.stop_menu_music()
@@ -10,4 +10,4 @@ func _ready():
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
-		get_tree().change_scene(next_world_scene)
+		get_tree().change_scene(next_scene)
