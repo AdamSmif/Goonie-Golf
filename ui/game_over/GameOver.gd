8dd84152737a3d1b59d8466e34371e93808b7b7a
AdamSmif <35468207+AdamSmif@users.noreply.github.com>
2021-08-07 10:45:04 -0700

    fixed disc golf course level transition and game over bug

--- ui/game_over/GameOver.gd
+++ ui/game_over/GameOver.gd
@@ -1,10 +1,15 @@
 extends Control
 
+# Game Over
+export(String, FILE, "*.tscn") var main_menu
+
 func _ready():
 
-#	yield(get_tree().create_timer(1), "timeout")
-	$AnimationPlayer.play("fade")
+	yield(get_tree().create_timer(1), "timeout")
 	$GameOverSound.play()
+	$AnimationPlayer.play("fade")
+	yield(get_tree().create_timer(3), "timeout")
+	get_tree().change_scene(main_menu)
 #	yield(get_tree().create_timer(4), "timeout")
 #	$AnimationPlayer.play_backwards("fade")
 #	yield(get_tree().create_timer(1), "timeout")
--- ui/game_over/PlayerKO.gd
+++ ui/game_over/PlayerKO.gd
@@ -1,11 +1,16 @@
 extends Control
 
+# Game Over
+export(String, FILE, "*.tscn") var game_over_scene
+
 func _ready():
 
-	yield(get_tree().create_timer(2), "timeout")
+	yield(get_tree().create_timer(1), "timeout")
 	$AnimationPlayer.play("playerkofade")
-	$KOSound.play()
-	yield(get_tree().create_timer(4), "timeout")
-	$AnimationPlayer.play_backwards("playerkofade")
-	get_tree().change_scene("res://UI/GameOver.tscn")
+	yield(get_tree().create_timer(3), "timeout")
+	get_tree().change_scene(game_over_scene)
+#	yield(get_tree().create_timer(4), "timeout")
+#	$AnimationPlayer.play_backwards("fade")
+#	yield(get_tree().create_timer(1), "timeout")
+#	get_tree().change_scene("res://UI/TitleScreen.tscn")
 	pass # Replace with function body
