704eb4b1d00532258d6af2b0e930d3dcdd8f6d84
AdamSmif <35468207+AdamSmif@users.noreply.github.com>
2021-08-14 14:15:32 -0700

    added village

--- entities/Player/top down golf cart player wade/GolfCartPlayer.gd
+++ entities/Player/top down golf cart player wade/GolfCartPlayer.gd
@@ -20,6 +20,32 @@ var score = 0
 var velocity = Vector2()
 
 func _physics_process(delta):
+# Spawn All Players
+	if Input.is_action_just_pressed("ui_accept"):
+		$AllOtheerPlayers/OtherPlayers2.visible = true
+		$AllOtheerPlayers/OtherPlayers3.visible = true
+		$AllOtheerPlayers/OtherPlayers4.visible = true
+		$AllOtheerPlayers/OtherPlayers5.visible = true
+		$AllOtheerPlayers/OtherPlayers6.visible = true
+		$AllOtheerPlayers/OtherPlayers7.visible = true
+		$AllOtheerPlayers/OtherPlayers8.visible = true
+# Spawn Other Players
+	if Input.is_action_just_pressed("spawn_2"):
+		$AllPlayers/OtherPlayers2.visible = true
+	elif Input.is_action_just_pressed("spawn_3"):
+		$AllPlayers/OtherPlayers3.visible = true
+	elif Input.is_action_just_pressed("spawn_4"):
+		$AllPlayers/OtherPlayers4.visible = true
+	elif Input.is_action_just_pressed("spawn_5"):
+		$AllPlayers/OtherPlayers5.visible = true
+	elif Input.is_action_just_pressed("spawn_6"):
+		$AllPlayers/OtherPlayers6.visible = true
+	elif Input.is_action_just_pressed("spawn_7"):
+		$AllPlayers/OtherPlayers7.visible = true
+	elif Input.is_action_just_pressed("spawn_8"):
+		$AllPlayers/OtherPlayers8.visible = true
+	
+	
 	velocity = Vector2()
 	if Input.is_action_pressed('right_%s' % id):
 		$Sprite.flip_h = false
