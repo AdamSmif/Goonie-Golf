91fefd81d0603f324ae39bb1c02282ffe33acdd2
AdamSmif <35468207+AdamSmif@users.noreply.github.com>
2021-09-21 16:43:36 -0700

    added level transition and fixed CanvasLayer bug

--- engine/Entrance/Entrance.gd
+++ engine/Entrance/Entrance.gd
@@ -12,4 +12,7 @@ export(String, FILE, "*.tscn") var next_world_scene
 
 func _on_Entrance_body_entered(_area: Area2D) -> void:
 	Global.from = get_parent().name 
+#	$BottleSound.play() 
+	$AnimationPlayer.play("transition")
+	yield(get_tree().create_timer(.50), "timeout")
 	get_tree().change_scene(next_world_scene)
--- entities/Assets/Disc Golf Basket/DiscGolfBasket.gd
+++ entities/Assets/Disc Golf Basket/DiscGolfBasket.gd
@@ -3,5 +3,12 @@ extends Area2D
 export(String, FILE, "*.tscn") var next_world_scene
 
 func _on_DiscGolfBasket_body_entered(_area: Area2D) -> void:
-	Global.from = get_parent().name 
+	Global.from = get_parent().name
+	$BottleSound.play() 
+	$AnimationPlayer.play("transition")
+	yield(get_tree().create_timer(1), "timeout")
 	get_tree().change_scene(next_world_scene)
+
+
+func _on_Timer_timeout():
+	pass # Replace with function body.
