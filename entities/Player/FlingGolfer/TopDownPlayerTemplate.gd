2ee8588b2c04757951a3ae31cd9ae91d0ba048e2
AdamSmif <35468207+AdamSmif@users.noreply.github.com>
2021-10-26 17:29:38 -0700

    configured for single player

--- entities/Player/FlingGolfer/TopDownPlayerTemplate.gd
+++ entities/Player/FlingGolfer/TopDownPlayerTemplate.gd
@@ -3,14 +3,18 @@ extends KinematicBody2D
 # Player Number
 export var id = 1
 export(int) var speed = 80.0
+onready var player_label = $Label
+onready var camera = $PlayerCamera
 
-# warning-ignore:unused_argument
+var motion = Vector2.ZERO
+
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength('right_%s' % id) - Input.get_action_strength('left_%s' % id)
 	input_vector.y = Input.get_action_strength('down_%s' % id) - Input.get_action_strength('up_%s' % id)
 	input_vector = input_vector.normalized()
-	
+		
 	if input_vector == Vector2.ZERO:
 		$AnimationTree.get("parameters/playback").travel("Idle")
 	else:
@@ -18,3 +22,7 @@ func _physics_process(delta):
 		move_and_slide(input_vector * speed)
 		$AnimationTree.set("parameters/Idle/blend_position", input_vector)
 		$AnimationTree.set("parameters/Walk/blend_position", input_vector)
+
+
+#func set_player_name ():
+#	player_label.text = Server.players[int(name)]["Player_name"]
--- levels/Levels.gd
+++ levels/Levels.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
 var player = preload("res://entities/Player/main player wade/Player.tscn")
+var HubPlayer = preload("res://entities/Player/HubPlayer/player.tscn")
+
 
 func _ready():
 	if Global.from != null:
 		get_node("Player").set_position(get_node(Global.from + "Pos").position)
+
