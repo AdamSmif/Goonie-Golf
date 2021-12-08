148a20052710426bfb99e31b81c9591218d9b725
AdamSmif <35468207+AdamSmif@users.noreply.github.com>
2021-12-07 21:39:23 -0800

    added prototype for hockey boss

---
+++ entities/Enemies/Hockey Boss/HockeyBoss.gd
@@ -0,0 +1,49 @@
+extends KinematicBody2D
+
+# Player Number
+var id = 1
+
+onready var stats = $Stats
+var motion = Vector2()
+var knockback = Vector2.ZERO
+
+export (int) var speed = 1200
+export (int) var jump_speed = -400
+export (int) var gravity = 2000
+
+var velocity = Vector2.ZERO
+
+func get_input():
+	velocity.x = 0
+
+func _physics_process(delta):
+	get_input()
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity, Vector2.UP)
+	if Input.is_action_just_pressed('up_%s' % id):
+		if is_on_floor():
+			$Sprite.play("jump")
+			velocity.y = jump_speed
+
+		if velocity.y > jump_speed: 
+			$Sprite.play("jump")
+		else:
+			$Sprite.play("fall")
+
+func _on_Hurtbox_area_entered(area):
+	stats.health -= 1
+	$AnimationPlayer.play("Hit")
+	$HitBadGuy.play()
+	knockback = Vector2.RIGHT * 150
+	knockback = Vector2.LEFT * 150
+
+func _on_Stats_no_health():
+	$Sprite.play("boom")
+	$ExplosionSound.play()
+	set_collision_layer_bit(4,false)
+	set_collision_mask_bit(0,false)
+	$Hurtbox.set_collision_layer_bit(4,false)
+	$Hurtbox.set_collision_mask_bit(0,false)
+	$Timer.start()
+
+
